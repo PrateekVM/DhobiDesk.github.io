34a88c7614a11c766582348f1c2520a7c039a5d4
PrateekVM <prateekmathapati748@gmail.com>
2026-04-03 18:28:12 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -37,12 +37,12 @@ import {
 
 // ⚠️ REPLACE THIS ENTIRE OBJECT WITH YOUR FIREBASE CONFIG ⚠️
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAdY967h56_3Js_z6vRMr7ilBjkJw93E_U",
+  authDomain: "dhobidesk.firebaseapp.com",
+  projectId: "dhobidesk",
+  storageBucket: "dhobidesk.firebasestorage.app",
+  messagingSenderId: "216057931395",
+  appId: "1:216057931395:web:c4ed3b0e54bbb9921b9db6"
 };
 
 // Initialize Firebase
